aff0973049558cd3a1a0471ed31fa0a17d569db8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-09-14 20:46:47 +0800

    feat: update to `kwaa.github.io` version

--- scripts/filter/after_post_render.js
+++ scripts/filter/after_post_render.js
@@ -1,4 +1,10 @@
-hexo.extend.filter.register('after_post_render', function (data) {
-    data.content = data.content.replace(/(<img [^>]*src="([^"]+)"[^>]*)/g, '$1' + 'class="mdui-img-rounded" loading="lazy"')
+hexo.extend.filter.register('after_post_render', data => {
+    data.content = data.content.replace(/<p>(<img [^>]*src="([^"]+)"[^>]*)><\/p>/g, '<figure class="mdui-img-fluid">' + '$1' + 'class="mdui-img-rounded" loading="lazy"></figure>')
+    data.content = data.content.replace(/\bvideo-container\b/g, 'mdui-video-container')
+    data.content = data.content.replace(/<table>/g, '<div class="mdui-table-fluid mdui-shadow-0"><table class="mdui-table mdui-table-hoverable">')
+    data.content = data.content.replace(/<\/table>/g, '</table></div>')
+    data.content = data.content.replace(/<h1 id="([^"]*)">([^<]*)<\/h1>/g, '<h1 class="mdui-valign" id="' + '$1' + '"><span>' + '$2' + '&nbsp;&nbsp;</span><a aria-hidden="true" class="mdui-btn mdui-btn-icon mdui-ripple" href="#' + '$1' + '"><i class="mdui-icon material-icons">link</i></a></h1>')
+    data.content = data.content.replace(/<h2 id="([^"]*)">([^<]*)<\/h2>/g, '<h2 class="mdui-valign" id="' + '$1' + '"><span>' + '$2' + '&nbsp;&nbsp;</span><a aria-hidden="true" class="mdui-btn mdui-btn-icon mdui-ripple" href="#' + '$1' + '"><i class="mdui-icon material-icons">link</i></a></h2>')
+    data.content = data.content.replace(/<h3 id="([^"]*)">([^<]*)<\/h3>/g, '<h3 class="mdui-valign" id="' + '$1' + '"><span>' + '$2' + '&nbsp;&nbsp;</span><a aria-hidden="true" class="mdui-btn mdui-btn-icon mdui-ripple" href="#' + '$1' + '"><i class="mdui-icon material-icons">link</i></a></h3>')
     return data;
 });
